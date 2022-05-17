3ebe4a1301c5c25c8187166e8ebd33ad2815ce2d
RanHarush <103585839+RanHarush@users.noreply.github.com>
2022-05-17 13:26:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var player1 = "images/dice" + randomNumber1 + ".png";
+var player2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", player1);
+document.querySelector(".img2").setAttribute("src", player2);
+
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "Draw";
+}
+else if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else {
+    document.querySelector(".container h1").innerHTML = "Player 2 Wins! 🚩";
+}
